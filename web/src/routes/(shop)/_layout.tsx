396c075e54829635067b942769bd2efe07e6536a
Diogo Falardo <diogofalardo04@gmail.com>
2026-02-18 12:32:20 +0000

    feat: delete shop action

--- web/src/routes/(shop)/_layout.tsx
+++ web/src/routes/(shop)/_layout.tsx
@@ -1,13 +1,16 @@
+import { createServerFn, useServerFn } from '@tanstack/react-start'
 import { useState } from 'react'
+import { and, eq } from 'drizzle-orm'
 import {
   Link,
   Outlet,
   createFileRoute,
   useLoaderData,
+  useRouter,
 } from '@tanstack/react-router'
-import { Settings, Store, Trash } from 'lucide-react'
+import { Settings, Store, Trash, Trash2Icon } from 'lucide-react'
+import { toast } from 'sonner'
 import { Button } from '@/components/ui/button'
-
 import {
   Dialog,
   DialogContent,
@@ -18,14 +21,86 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import ShopUpdate from '@/components/shop/shopUpdate'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogMedia,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import {
+  userShopOwnershipSchema,
+  validateUserShopOwnership,
+} from '@/server/user/user.server'
+import { db } from '@/db'
+import { shops } from '@/db/schema'
+import { useQueryClient } from '@tanstack/react-query'
 
 export const Route = createFileRoute('/(shop)/_layout')({
   component: RouteComponent,
 })
 
+const deleteShop = createServerFn({ method: 'POST' })
+  .inputValidator((data: { shopId: string; userId: string }) => data)
+  .handler(async ({ data }) => {
+    const ids = {
+      userId: data.userId,
+      shopId: data.shopId,
+    }
+
+    userShopOwnershipSchema.parse(ids)
+
+    const isOwner = await validateUserShopOwnership(ids)
+
+    if (!isOwner) {
+      throw new Error('not authorized')
+    }
+
+    try {
+      await db
+        .delete(shops)
+        .where(and(eq(shops.userId, data.userId), eq(shops.id, data.shopId)))
+
+      return true
+    } catch (err: any) {
+      console.error(err.message)
+      toast.error(err.message ?? 'Error while deleting shop')
+    }
+  })
+
 function RouteComponent() {
   const data = useLoaderData({ from: '/(shop)/_layout/dashboard/$id' })
   const [open, setOpen] = useState(false)
+  // required hooks
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  const deleted = useServerFn(deleteShop)
+
+  const deleteUserShop = async () => {
+    try {
+      const result = await deleted({
+        data: {
+          userId: data.shopInfo.userId,
+          shopId: data.shopInfo.id,
+        },
+      })
+      if (result) {
+        toast.success('Shop and its related data got deleted!')
+        // invalidate all querys related to the shop
+        queryClient.invalidateQueries({ queryKey: ['shop'] })
+        router.invalidate()
+
+        router.navigate({ to: '/' })
+      }
+    } catch (err: any) {
+      toast.error(err.message ?? 'Error while deleting shop')
+    }
+  }
 
   return (
     <div className="min-h-screen grid grid-rows-[auto,1fr,auto]">
@@ -40,7 +115,7 @@ function RouteComponent() {
             </Link>
             <div className="flex items-center gap-3">
               <Dialog open={open} onOpenChange={setOpen}>
-                <DialogTrigger>
+                <DialogTrigger asChild>
                   <Button
                     variant={'outline'}
                     size={'icon-sm'}
@@ -61,10 +136,40 @@ function RouteComponent() {
                     onSuccess={() => setOpen(false)}
                   />
                   <DialogFooter>
-                    <Button variant={'destructive'}>
-                      <Trash />
-                      Delete Shop
-                    </Button>
+                    {/* delete dialog */}
+                    <AlertDialog>
+                      <AlertDialogTrigger asChild>
+                        <Button variant={'destructive'}>
+                          <Trash />
+                          Delete Shop
+                        </Button>
+                      </AlertDialogTrigger>
+                      <AlertDialogContent size="sm">
+                        <AlertDialogHeader>
+                          <AlertDialogMedia className="bg-destructive">
+                            <Trash2Icon />
+                          </AlertDialogMedia>
+                          <AlertDialogTitle>
+                            Delete shop?
+                            <AlertDialogDescription>
+                              This will delete this shop and all respective
+                              data!
+                            </AlertDialogDescription>
+                          </AlertDialogTitle>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                          <AlertDialogCancel variant="outline">
+                            Cancel
+                          </AlertDialogCancel>
+                          <AlertDialogAction
+                            variant="destructive"
+                            onClick={() => deleteUserShop()}
+                          >
+                            Delete
+                          </AlertDialogAction>
+                        </AlertDialogFooter>
+                      </AlertDialogContent>
+                    </AlertDialog>
                     <Button form="update-shop-form" type="submit">
                       <Store />
                       Update Shop
