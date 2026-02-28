8ecb3fa4b8b6776ae7db169ffcfef9550a463f9e
Diogo Falardo <diogofalardo04@gmail.com>
2026-02-28 09:08:21 +0000

    test: Open Dashboard inside telegram

--- web/src/routes/(shop)/_layout.dashboard.$id.tsx
+++ web/src/routes/(shop)/_layout.dashboard.$id.tsx
@@ -29,57 +29,52 @@ export function getTelegramInitData() {
 export const shopLoader = createServerFn({ method: 'GET' })
   .inputValidator((data: { shopId: string; initData?: string }) => data)
   .handler(async ({ data }) => {
-    // if (!data.initData)
-    //   throw new Error('What are you looking for couldnt be found')
+    if (!data.initData)
+      throw new Error('What are you looking for couldnt be found')
 
     if (!data.shopId)
       throw new Error('What are you looking for couldnt be found')
 
     // if there is data
-    // if (typeof data.initData === 'string' && data.initData.length > 0) {
-    //   // gets tgUser Object
-    //   const tgUser = await telegramVerification({
-    //     data: { initData: data.initData },
-    //   })
-    //   const uuidValidation = z.uuid().safeParse(tgUser.userId)
-    //   if (uuidValidation.success) {
-    //     // userId validated
-    //     const userId = uuidValidation.data
-    //     // shop info from telegram
-    //     try {
-    //       const shopInfo = await getUserShopInfo({
-    //         data: { userId: userId, shopId: data.shopId },
-    //       })
-    //       return { shopInfo: shopInfo }
-    //     } catch (err: any) {
-    //       throw new Error(err.message)
-    //     }
-    //   }
-    // }
-    try {
-      const shopInfo = await getUserShopInfo({
-        data: {
-          userId: 'bf8d62b5-08f3-11f1-a9f8-644ed72189d4',
-          shopId: data.shopId,
-        },
+    if (typeof data.initData === 'string' && data.initData.length > 0) {
+      // gets tgUser Object
+      const tgUser = await telegramVerification({
+        data: { initData: data.initData },
       })
-
-      console.log('SHOP INFO', shopInfo)
-
-      return { shopInfo: shopInfo }
-    } catch (err: any) {
-      console.error(err)
-      throw new Error(err.message)
+      const uuidValidation = z.uuid().safeParse(tgUser.userId)
+      if (uuidValidation.success) {
+        // userId validated
+        const userId = uuidValidation.data
+        // shop info from telegram
+        try {
+          const shopInfo = await getUserShopInfo({
+            data: { userId: userId, shopId: data.shopId },
+          })
+          return { shopInfo: shopInfo }
+        } catch (err: any) {
+          throw new Error(err.message)
+        }
+      }
     }
+    // try {
+    //   const shopInfo = await getUserShopInfo({
+    //     data: {
+    //       userId: 'bf8d62b5-08f3-11f1-a9f8-644ed72189d4',
+    //       shopId: data.shopId,
+    //     },
+    //   })
+
+    //   console.log('SHOP INFO', shopInfo)
+
+    //   return { shopInfo: shopInfo }
+    // } catch (err: any) {
+    //   console.error(err)
+    //   throw new Error(err.message)
+    // }
   })
 
 export function DashboardErrorComponent({ error }: { error: Error }) {
-  return (
-    <ErrorWrapper
-      errorTitle={error.message}
-      errorDescription="Probably this shop doesnt exist anymore!"
-    />
-  )
+  return <ErrorWrapper errorTitle={error.message} errorDescription="" />
 }
 
 export const Route = createFileRoute('/(shop)/_layout/dashboard/$id')({
@@ -93,11 +88,17 @@ export const Route = createFileRoute('/(shop)/_layout/dashboard/$id')({
 })
 
 function RouteComponent() {
-  const { shopInfo } = Route.useLoaderData()
+  const loaderData = Route.useLoaderData()
 
-  if (!shopInfo) {
+  if (!loaderData || !loaderData.shopInfo) {
+    return (
+      <div className="w-full h-full flex justify-center items-center">
+        <Spinner /> Loading....
+      </div>
+    )
   }
 
+  const { shopInfo } = loaderData
   const { userId, id: shopId } = shopInfo
 
   const { data, isLoading } = useGetShopProducts({ userId, shopId })
