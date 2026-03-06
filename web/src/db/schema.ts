31ec9972bbdf1ef3afb5984895fbcf88cf05f7a1
Diogo Falardo <diogofalardo04@gmail.com>
2026-03-06 10:06:37 +0000

    fix initialize database schema

--- web/src/db/schema.ts
+++ web/src/db/schema.ts
@@ -1,19 +1,20 @@
+import { table } from 'console'
 import {
-  mysqlTable,
-  mysqlSchema,
-  tinyint,
   AnyMySqlColumn,
-  foreignKey,
-  primaryKey,
-  char,
-  varchar,
-  index,
-  decimal,
-  text,
-  datetime,
-  mysqlEnum,
-  unique,
   bigint,
+  char,
+  datetime,
+  decimal,
+  index,
+  foreignKey,
+  tinyint,
+  unique,
+  varchar,
+  text,
+  primaryKey,
+  mysqlSchema,
+  mysqlTable,
+  mysqlEnum,
 } from 'drizzle-orm/mysql-core'
 import { sql } from 'drizzle-orm'
 
@@ -98,5 +99,86 @@ export const users = mysqlTable(
   (table) => [
     primaryKey({ columns: [table.id], name: 'users_id' }),
     unique('uq_users_telegram_user_id').on(table.telegramUserId),
+  ],
+)
+
+export const shippingMethods = mysqlTable(
+  'shipping_methods',
+  {
+    id: char({ length: 36 })
+      .default(sql`(uuid())`)
+      .notNull(),
+    shopId: char('shop_id', { length: 36 })
+      .notNull()
+      .references(() => shops.id, { onDelete: 'cascade' }),
+    method: varchar('method', { length: 255 }).notNull(),
+  },
+  (table) => [
+    primaryKey({ columns: [table.id], name: 'shippingMethods_id' }),
+    index('idx_shippingMethods_shop_id').on(table.shopId),
+  ],
+)
+
+export const paymentMethods = mysqlTable(
+  'payment_methods',
+  {
+    id: char({ length: 36 })
+      .default(sql`(uuid())`)
+      .notNull(),
+    shopId: char('shop_id', { length: 36 })
+      .notNull()
+      .references(() => shops.id, { onDelete: 'cascade' }),
+    method: varchar('method', { length: 255 }).notNull(),
+  },
+  (table) => [
+    primaryKey({ columns: [table.id], name: 'paymentMethods_id' }),
+    index('idx_paymentMethods_shop_id').on(table.shopId),
+  ],
+)
+
+export const orders = mysqlTable(
+  'orders',
+  {
+    id: char({ length: 36 })
+      .default(sql`(uuid())`)
+      .notNull(),
+    shopId: char('shop_id', { length: 36 })
+      .notNull()
+      .references(() => shops.id, { onDelete: 'cascade' }),
+    orderStatus: varchar('order_status', { length: 120 }).notNull(),
+    orderCustomMessage: varchar('order_custom_message', { length: 2500 }),
+    orderShippingMethod: char('order_shipping_method', { length: 36 })
+      .notNull()
+      .references(() => shippingMethods.id, { onDelete: 'cascade' }),
+    orderPaymentMethod: char('order_payment_method', { length: 36 })
+      .notNull()
+      .references(() => paymentMethods.id, { onDelete: 'cascade' }),
+  },
+  (table) => [
+    primaryKey({ columns: [table.id], name: 'orders_id' }),
+    index('idx_orders_shop_id').on(table.shopId),
+    index('idx_orders_status').on(table.orderStatus),
+    index('idx_orders_shipping_method').on(table.orderShippingMethod),
+    index('idx_orders_payment_method').on(table.orderPaymentMethod),
+  ],
+)
+
+export const productsOrders = mysqlTable(
+  'products_orders',
+  {
+    id: char({ length: 36 })
+      .default(sql`(uuid())`)
+      .notNull(),
+    orderId: char('order_id', { length: 36 })
+      .notNull()
+      .references(() => orders.id, { onDelete: 'cascade' }),
+    productId: char('product_id', { length: 36 })
+      .notNull()
+      .references(() => products.id, { onDelete: 'cascade' }),
+  },
+  (table) => [
+    primaryKey({ columns: [table.id], name: 'productsOrders_id' }),
+    index('idx_orders_id').on(table.orderId),
+    index('idx_orders_product_id').on(table.productId),
   ],
 )
